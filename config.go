0ff341f82087254c3241517e4bd7e9c67079c3e4
3nprob <3nprob@3nprob>
2021-08-23 12:23:14 +0900

    tor: Allow supplying proxy credentials

--- config.go
+++ config.go
@@ -19,6 +19,8 @@ import (
 	"strings"
 	"time"
 
+	"golang.org/x/net/proxy"
+
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
 	flags "github.com/jessevdk/go-flags"
@@ -1015,9 +1017,18 @@ func ValidateConfig(cfg Config, interceptor signal.Interceptor, fileParser,
 			cfg.MaxCommitFeeRateAnchors)
 	}
 
+	socksAuth := &proxy.Auth{}
 	if cfg.SOCKS != "" {
+		socksAddress := cfg.SOCKS
+		if strings.Contains(socksAddress, "@") {
+			parts := strings.Split(socksAddress, "@")
+			socksAddress = parts[1]
+			creds := strings.Split(parts[0], ":")
+			socksAuth.User = creds[0]
+			socksAuth.Password = creds[1]
+		}
 		socks, err := lncfg.ParseAddressString(
-			cfg.SOCKS, "",
+			socksAddress, "",
 			cfg.net.ResolveTCPAddr,
 		)
 		if err != nil {
@@ -1110,6 +1121,7 @@ func ValidateConfig(cfg Config, interceptor signal.Interceptor, fileParser,
 
 	clearNet := &tor.ClearNet{
 		SOCKS:          cfg.SOCKS,
+		SOCKSAuth:      socksAuth,
 		NoProxyTargets: cfg.NoProxyTargets,
 	}
 	if cfg.Tor.Active {
